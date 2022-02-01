de77e5a338ca9379cf8f2acd02bc1cf5196b7dbb
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-01 21:44:31 +0200

    Add more styling to the popup

--- main.js
+++ main.js
@@ -1,49 +1,45 @@
-const menuList = document.querySelector(".mop_menu");
-const menuIcon = document.querySelector("#menuIcon");
-const xIcone = document.querySelector("#xIcon");
-const popupXBtn = document.querySelector("#xIconPopup");
-const seeMoreBtn = document.querySelectorAll(".seeProject");
-const popup = document.querySelector(".popupWindow");
+const menuList = document.querySelector('.mop_menu');
+const menuIcon = document.querySelector('#menuIcon');
+const xIcone = document.querySelector('#xIcon');
+const popupXBtn = document.querySelector('#xIconPopup');
+const seeMoreBtn = document.querySelectorAll('.seeProject');
+const popup = document.querySelector('.popupWindow')
 const body = document.body;
-let option = document.querySelectorAll(".listOption");
+let option = document.querySelectorAll('.listOption');
 
 option = Array.from(option);
 
-menuIcon.addEventListener("click", () => {
-  menuList.classList.remove("menu-hidden");
-  menuList.classList.add("menu-visible");
-  body.classList.toggle("no-scroll");
+menuIcon.addEventListener('click', () => {
+    menuList.classList.remove('menu-hidden');
+    menuList.classList.add('menu-visible');
+    body.classList.toggle('no-scroll');
+
 });
 
-xIcone.addEventListener("click", () => {
-  menuList.classList.add("menu-hidden");
-  menuList.classList.remove("menu-visible");
-  body.classList.toggle("no-scroll");
+
+xIcone.addEventListener('click', () => {
+    menuList.classList.add('menu-hidden');
+    menuList.classList.remove('menu-visible');
+    body.classList.toggle('no-scroll');
 });
 
-option.forEach((item) => {
-  item.addEventListener("click", () => {
-    //handle click
-    menuList.classList.add("menu-hidden");
-    menuList.classList.remove("menu-visible");
-    body.classList.toggle("no-scroll");
-  });
+option.forEach(item => {
+    item.addEventListener('click', () => {
+      //handle click
+    menuList.classList.add('menu-hidden');
+    menuList.classList.remove('menu-visible');
+    body.classList.toggle('no-scroll');
+    })
 });
 
-seeMoreBtn.forEach((item) => {
-  item.addEventListener("click", () => {
-    if (document.body.clientWidth ===992) {
-        popup.classList.toggle("d-none");
-        body.classList.toggle("no-scroll");
-      
-    } else {
-        popup.classList.toggle("d-none");
-        body.classList.toggle("no-scroll");
-    }
-    
+seeMoreBtn.forEach(item => {
+    item.addEventListener('click', () => {
+    body.classList.toggle('no-scroll');
+    popup.classList.toggle('d-none');
     });
 });
 
-// popupXBtn.addEventListener("hover", () => {
-//   console.log("sdf");
-// });
+popupXBtn.addEventListener('click', () => {
+    popup.classList.toggle('d-none');
+    body.classList.toggle('no-scroll');    
+})
