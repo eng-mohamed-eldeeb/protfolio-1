9ec2e64ba393cdf5cf300d3d06fdcbdf0e66a69e
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-02 17:33:03 +0200

    Fix main linter errors

--- main.js
+++ main.js
@@ -4,24 +4,24 @@ const wrapper = document.querySelector(".wrapper");
 const iconX = document.querySelector("#iconX");
 const options = document.querySelectorAll(".options");
 
-[menuIcon ,iconX].forEach(e => {
-    e.addEventListener("click", () => {
-        menuContaner.classList.toggle('d-none');
-        menuContaner.classList.toggle('p-fixed');
-        menuContaner.classList.toggle("d-flex");
-        wrapper.classList.toggle("p-fixed");
-        wrapper.classList.toggle("d-none");
-        wrapper.classList.toggle("d-flex");
-      });      
+[menuIcon, iconX].forEach((e) => {
+  e.addEventListener("click", () => {
+    menuContaner.classList.toggle("d-none");
+    menuContaner.classList.toggle("p-fixed");
+    menuContaner.classList.toggle("d-flex");
+    wrapper.classList.toggle("p-fixed");
+    wrapper.classList.toggle("d-none");
+    wrapper.classList.toggle("d-flex");
+  });
 });
 
 options.forEach((e) => {
-    e.addEventListener("click", () => {
-        menuContaner.classList.toggle('d-none');
-        menuContaner.classList.toggle('p-fixed');
-        menuContaner.classList.toggle("d-flex");
-        wrapper.classList.toggle("p-fixed");
-        wrapper.classList.toggle("d-none");
-        wrapper.classList.toggle("d-flex");
-    })
-})
+  e.addEventListener("click", () => {
+    menuContaner.classList.toggle("d-none");
+    menuContaner.classList.toggle("p-fixed");
+    menuContaner.classList.toggle("d-flex");
+    wrapper.classList.toggle("p-fixed");
+    wrapper.classList.toggle("d-none");
+    wrapper.classList.toggle("d-flex");
+  });
+});
