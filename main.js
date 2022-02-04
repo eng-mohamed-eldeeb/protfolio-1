4b3af9e81c7ecd042ad14f7d4ca68c08d744b466
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-04 15:02:59 +0200

    Fix linter error

--- main.js
+++ main.js
@@ -1,74 +1,74 @@
-const menuIcon = document.querySelector("#menuIcon");
-const menuContaner = document.querySelector(".menu_contaner");
-const wrapper = document.querySelector(".wrapper");
-const options = document.querySelectorAll(".options");
-
-const seeBtn = document.querySelectorAll(".SEE");
+const menuIcon = document.querySelector('#menuIcon');
+const menuContaner = document.querySelector('.menu_contaner');
+const wrapper = document.querySelector('.wrapper');
+const options = document.querySelectorAll('.options');
+const iconX = document.querySelector('.iconX');
+const seeBtn = document.querySelectorAll('.SEE');
 
 [menuIcon, iconX].forEach((e) => {
-  e.addEventListener("click", () => {
-    menuContaner.classList.toggle("d-none");
-    menuContaner.classList.toggle("p-fixed");
-    menuContaner.classList.toggle("d-flex");
-    wrapper.classList.toggle("p-fixed");
-    wrapper.classList.toggle("d-none");
-    wrapper.classList.toggle("d-flex");
-    document.body.classList.toggle("no-scroll");
+  e.addEventListener('click', () => {
+    menuContaner.classList.toggle('d-none');
+    menuContaner.classList.toggle('p-fixed');
+    menuContaner.classList.toggle('d-flex');
+    wrapper.classList.toggle('p-fixed');
+    wrapper.classList.toggle('d-none');
+    wrapper.classList.toggle('d-flex');
+    document.body.classList.toggle('no-scroll');
   });
 });
 
 options.forEach((e) => {
-  e.addEventListener("click", () => {
-    menuContaner.classList.toggle("d-none");
-    menuContaner.classList.toggle("p-fixed");
-    menuContaner.classList.toggle("d-flex");
-    wrapper.classList.toggle("p-fixed");
-    wrapper.classList.toggle("d-none");
-    wrapper.classList.toggle("d-flex");
-    document.body.classList.toggle("no-scroll");
+  e.addEventListener('click', () => {
+    menuContaner.classList.toggle('d-none');
+    menuContaner.classList.toggle('p-fixed');
+    menuContaner.classList.toggle('d-flex');
+    wrapper.classList.toggle('p-fixed');
+    wrapper.classList.toggle('d-none');
+    wrapper.classList.toggle('d-flex');
+    document.body.classList.toggle('no-scroll');
   });
 });
 
 const dataArray = [
   {
     id: 0,
-    imgUlr: "./img/workPhoto.png",
-    name: "Tonic",
-    technologies: ["CANOPY", "BACK END DEV", "2015"],
+    imgUlr: './img/workPhoto.png',
+    name: 'Tonic',
+    technologies: ['CANOPY', 'BACK END DEV', '2015'],
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?",
-    language: ["HTML", "CSS", "JS"],
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?',
+    language: ['HTML', 'CSS', 'JS'],
   },
   {
     id: 1,
-    imgUlr: "./img/workPhoto.png",
-    name: "Tonic",
-    technologies: ["CANOPY", "BACK END DEV", "2015"],
+    imgUlr: './img/workPhoto.png',
+    name: 'Tonic',
+    technologies: ['CANOPY', 'BACK END DEV', '2015'],
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?",
-    language: ["HTML", "CSS", "JS"],
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?',
+    language: ['HTML', 'CSS', 'JS'],
   },
   {
     id: 2,
-    imgUlr: "./img/workPhoto.png",
-    name: "Tonic",
-    technologies: ["CANOPY", "BACK END DEV", "2015"],
+    imgUlr: './img/workPhoto.png',
+    name: 'Tonic',
+    technologies: ['CANOPY', 'BACK END DEV', '2015'],
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?",
-    language: ["HTML", "CSS", "JS"],
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?',
+    language: ['HTML', 'CSS', 'JS'],
   },
   {
     id: 3,
-    imgUlr: "./img/workPhoto.png",
-    name: "Tonic",
-    technologies: ["CANOPY", "BACK END DEV", "2015"],
+    imgUlr: './img/workPhoto.png',
+    name: 'Tonic',
+    technologies: ['CANOPY', 'BACK END DEV', '2015'],
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?",
-    language: ["HTML", "CSS", "JS"],
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?',
+    language: ['HTML', 'CSS', 'JS'],
   },
 ];
 
-const popupCard = document.getElementById("popupCard");
+const popupCard = document.getElementById('popupCard');
 
 function createPopupCard(popupData) {
   const data = `
@@ -115,26 +115,25 @@ function createPopupCard(popupData) {
   </div>
 `;
 
-  const popupWrapper = document.createElement("div");
-  const card = document.querySelector(".popup-card");
+  const popupWrapper = document.createElement('div');
 
-  popupWrapper.setAttribute("class", "popupwrapper");
+  popupWrapper.setAttribute('class', 'popupwrapper');
 
   popupWrapper.innerHTML += data;
 
-  popupWrapper.querySelector("#xIcon").addEventListener("click", () => {
-    popupWrapper.innerHTML = "";
-    document.body.classList.toggle("no-scroll");
+  popupWrapper.querySelector('#xIcon').addEventListener('click', () => {
+    popupWrapper.innerHTML = '';
+    document.body.classList.toggle('no-scroll');
   });
 
   return popupWrapper;
 }
 
 seeBtn.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
+  btn.addEventListener('click', (e) => {
     const popupData = dataArray[e.target.id];
     const popup = createPopupCard(popupData);
     popupCard.append(popup);
-    document.body.classList.toggle("no-scroll");
+    document.body.classList.toggle('no-scroll');
   });
 });
