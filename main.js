ae1aad558dc1c420f69124702879dee2a3df449a
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-04 18:55:20 +0200

    Fix the 1st and the 2nd error

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const wrapper = document.querySelector('.wrapper');
 const options = document.querySelectorAll('.options');
 const iconX = document.querySelector('#iconX');
 const seeBtn = document.querySelectorAll('.SEE');
+const work = document.querySelector('#works');
 
 [menuIcon, iconX].forEach((e) => {
   e.addEventListener('click', () => {
@@ -38,33 +39,41 @@ const dataArray = [
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?',
     language: ['HTML', 'CSS', 'JS'],
+    giturl: 'https://github.com/eng-mohamed-eldeeb',
+    source: 'https://eng-mohamed-eldeeb.github.io/protofolio/',
   },
   {
     id: 1,
-    imgUlr: './img/workPhoto.png',
+    imgUlr: './img/workPhoto1.png',
     name: 'Tonic',
     technologies: ['CANOPY', 'BACK END DEV', '2015'],
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?',
     language: ['HTML', 'CSS', 'JS'],
+    giturl: 'https://github.com/eng-mohamed-eldeeb',
+    source: 'https://eng-mohamed-eldeeb.github.io/protofolio/',
   },
   {
     id: 2,
-    imgUlr: './img/workPhoto.png',
+    imgUlr: './img/workPoto2.png',
     name: 'Tonic',
     technologies: ['CANOPY', 'BACK END DEV', '2015'],
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?',
     language: ['HTML', 'CSS', 'JS'],
+    giturl: 'https://github.com/eng-mohamed-eldeeb',
+    source: 'https://eng-mohamed-eldeeb.github.io/protofolio/',
   },
   {
     id: 3,
-    imgUlr: './img/workPhoto.png',
+    imgUlr: './img/workPoto3.png',
     name: 'Tonic',
     technologies: ['CANOPY', 'BACK END DEV', '2015'],
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos  pariatur voluptas, amet autem natus quam vitae culpa ut! Voluptate officiis eligendi non atque? Ab necessitatibus, ipsa aliquam provident, odio incidunt eum debitis ipsum non animi consecteturomnis, labore eius quo dolores rerum nobis vel recusandae saepe fuga sed libero perferendis?',
     language: ['HTML', 'CSS', 'JS'],
+    giturl: 'https://github.com/eng-mohamed-eldeeb',
+    source: 'https://eng-mohamed-eldeeb.github.io/protofolio/',
   },
 ];
 
@@ -96,14 +105,14 @@ function createPopupCard(popupData) {
         <hr />
         <div class="popupBtns button">
           <button class="popupBtnn" type="button">
-            <a href="">
+            <a href="${popupData.source}">
               <p>See live</p>
               <i class="fas fa-share-square"></i>
             </a>
           </button>
           <button class="popupBtnn" type="button">
-            <a href="">
-              <p>See live</p>
+            <a href="${popupData.giturl}">
+              <p>See cource</p>
               <i class="fab fa-github"></i>
             </a>
           </button>
@@ -137,3 +146,50 @@ seeBtn.forEach((btn) => {
     document.body.classList.toggle('no-scroll');
   });
 });
+
+function createWork(popupData) {
+  const data = `
+  <div class="project">
+  <img src="${popupData.imgUlr}" alt="work photo one" />
+  <div class="project-info">
+    <h3>${popupData.name}</h3>
+    <div class="in-body">
+    <p class="a7a">${popupData.technologies[0]}</p>
+    <p>${popupData.technologies[1]}</p>
+    <p>${popupData.technologies[2]}</p>
+    </div>
+    <p class="dis--">
+      ${popupData.description}
+    </p>
+    <ul class="butt">
+    <li class="ppp">${popupData.language[0]}</li>
+    <li class="ppp">${popupData.language[1]}</li>
+    <li class="ppp">${popupData.language[2]}</li>
+    </ul>
+    <button id="0" class="SEE" type="button">See Project</button>
+  </div>
+</div>
+`;
+
+  const popupWrapper = document.createElement('div');
+
+  popupWrapper.setAttribute('class', 'project');
+
+  popupWrapper.innerHTML += data;
+
+  popupWrapper.querySelector('.SEE').addEventListener('click', (e) => {
+    const popupData = dataArray[e.target.id];
+    const popup = createPopupCard(popupData);
+    popupCard.append(popup);
+    document.body.classList.toggle('no-scroll');
+  });
+
+  return popupWrapper;
+}
+
+for (let i = 0; i < 4; i +=1) {
+  const popupData = dataArray[i];
+  const popup = createWork(popupData);
+  console.log(popup);
+  work.append(popup);
+}
