f0241afea80dca3fa513c89a6a2ba4908bafd10f
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-04 14:39:50 +0200

    Add an eventListner to the "x" icon

--- main.js
+++ main.js
@@ -1,50 +1,33 @@
-// const menuIcon = document.querySelector("#menuIcon");
-// const menuContaner = document.querySelector(".menu_contaner");
-// const wrapper = document.querySelector(".wrapper");
-// const iconX = document.querySelector("#iconX");
-// const options = document.querySelectorAll(".options");
+const menuIcon = document.querySelector("#menuIcon");
+const menuContaner = document.querySelector(".menu_contaner");
+const wrapper = document.querySelector(".wrapper");
+const options = document.querySelectorAll(".options");
+
 const seeBtn = document.querySelectorAll(".SEE");
-const xIcon = `<i id="popiconX" class="fas fa-lg fa-times"></i>`;
 
-// [menuIcon, iconX].forEach((e) => {
-//   e.addEventListener("click", () => {
-//     menuContaner.classList.toggle("d-none");
-//     menuContaner.classList.toggle("p-fixed");
-//     menuContaner.classList.toggle("d-flex");
-//     wrapper.classList.toggle("p-fixed");
-//     wrapper.classList.toggle("d-none");
-//     wrapper.classList.toggle("d-flex");
-//     document.body.classList.toggle("no-scroll");
-//   });
-// });
+[menuIcon, iconX].forEach((e) => {
+  e.addEventListener("click", () => {
+    menuContaner.classList.toggle("d-none");
+    menuContaner.classList.toggle("p-fixed");
+    menuContaner.classList.toggle("d-flex");
+    wrapper.classList.toggle("p-fixed");
+    wrapper.classList.toggle("d-none");
+    wrapper.classList.toggle("d-flex");
+    document.body.classList.toggle("no-scroll");
+  });
+});
 
-// options.forEach((e) => {
-//   e.addEventListener("click", () => {
-//     menuContaner.classList.toggle("d-none");
-//     menuContaner.classList.toggle("p-fixed");
-//     menuContaner.classList.toggle("d-flex");
-//     wrapper.classList.toggle("p-fixed");
-//     wrapper.classList.toggle("d-none");
-//     wrapper.classList.toggle("d-flex");
-//     document.body.classList.toggle("no-scroll");
-//   });
-// });
-
-// seeBtn.forEach((e) => {
-//   e.addEventListener("click", () => {
-//     popupWindow.classList.toggle("d-none");
-//     popupWindow.classList.toggle("d-flex");
-//     popupWindow.classList.toggle("p-fixed");
-//     document.body.classList.toggle("no-scroll");
-//   });
-// });
-
-// popiconX.addEventListener("click", () => {
-//   popupWindow.classList.toggle("d-none");
-//   popupWindow.classList.toggle("d-flex");
-//   popupWindow.classList.toggle("p-fixed");
-//   document.body.classList.toggle("no-scroll");
-// });
+options.forEach((e) => {
+  e.addEventListener("click", () => {
+    menuContaner.classList.toggle("d-none");
+    menuContaner.classList.toggle("p-fixed");
+    menuContaner.classList.toggle("d-flex");
+    wrapper.classList.toggle("p-fixed");
+    wrapper.classList.toggle("d-none");
+    wrapper.classList.toggle("d-flex");
+    document.body.classList.toggle("no-scroll");
+  });
+});
 
 const dataArray = [
   {
@@ -92,8 +75,8 @@ function createPopupCard(popupData) {
   <div class="popupContaner d-flex">
   <div class="popupwrapper">
   <div class="project popup-card d-flex">
-  ${xIcon}
-    <h1>${popupData.name}</h1>
+  <i id="xIcon" class="fas fa-lg fa-times"></i>
+        <h1>${popupData.name}</h1>
     <div class="in-body">
       <p class="a7a">${popupData.technologies[0]}</p>
       <p>${popupData.technologies[1]}</p>
@@ -137,24 +120,21 @@ function createPopupCard(popupData) {
 
   popupWrapper.setAttribute("class", "popupwrapper");
 
-  popupWrapper.innerHTML = data;
+  popupWrapper.innerHTML += data;
 
-  // card.innerHTML += xIcon;
+  popupWrapper.querySelector("#xIcon").addEventListener("click", () => {
+    popupWrapper.innerHTML = "";
+    document.body.classList.toggle("no-scroll");
+  });
+
   return popupWrapper;
 }
 
 seeBtn.forEach((btn) => {
-  // console.log(btn);
   btn.addEventListener("click", (e) => {
-    if (!document.querySelector(".popupWrapper")) {
-      const popupData = dataArray[e.target.id];
-      const popup = createPopupCard(popupData);
-      popupCard.append(popup);
-      document.body.classList.toggle("no-scroll");
-    }
+    const popupData = dataArray[e.target.id];
+    const popup = createPopupCard(popupData);
+    popupCard.append(popup);
+    document.body.classList.toggle("no-scroll");
   });
-});
-
-xIcon.addEventListener("click", (e) => {
-  console.log("sda");
 });
